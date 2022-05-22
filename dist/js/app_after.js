ce5b34bfe854b112dcc29ecd44da08d203f9bb8a
Дмитрий Мальцев <dabags@ya.ru>
2022-05-22 17:05:10 +0500

    [repair] (feat) completed repair page

--- dist/js/app_after.js
+++ dist/js/app_after.js
@@ -16390,7 +16390,7 @@ PERFORMANCE OF THIS SOFTWARE.
             const connectInput2 = document.querySelector(".computer-tariff__input--server");
             const connectSlider1 = document.querySelector(".computer-tariff__range--computer");
             const connectSlider2 = document.querySelector(".computer-tariff__range--server");
-            const tariffSlider1 = nouislider.create(connectSlider1, {
+            const tariffSlider1 = connectSlider1 && nouislider.create(connectSlider1, {
                 tooltips: true,
                 connect: [ true, false ],
                 step: 1,
@@ -16408,7 +16408,7 @@ PERFORMANCE OF THIS SOFTWARE.
                     density: 400
                 }
             });
-            const tariffSlider2 = nouislider.create(connectSlider2, {
+            const tariffSlider2 = connectSlider2 && nouislider.create(connectSlider2, {
                 tooltips: true,
                 connect: [ true, false ],
                 step: 1,
@@ -16426,8 +16426,8 @@ PERFORMANCE OF THIS SOFTWARE.
                     density: 400
                 }
             });
-            tariffSlider1.on("update", (value => connectInput1.value = value));
-            tariffSlider2.on("update", (value => connectInput2.value = value));
+            tariffSlider1 && connectInput1 && tariffSlider1.on("update", (value => connectInput1.value = value));
+            tariffSlider2 && connectInput2 && tariffSlider2.on("update", (value => connectInput2.value = value));
         }));
         window["FLS"] = true;
         isWebp();
--- src/js/computer.js
+++ src/js/computer.js
@@ -7,46 +7,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const connectSlider1 = document.querySelector('.computer-tariff__range--computer');
   const connectSlider2 = document.querySelector('.computer-tariff__range--server');
 
-  if (connectSlider1 && connectSlider2 && connectInput1 && connectInput2) {
-    const tariffSlider1 = nouislider.create(connectSlider1, {
-      tooltips: true,
-      connect: [ true, false ],
-      step: 1,
-      start: 10,
-      range: {
-        "min": 0,
-        "max": 40
-      },
-      format: {
-        to: (value) => value,
-        from: (value) => Math.round(Number(value)),
-      },
-      pips: {
-        mode: "range",
-        density: 400,
-      },
-    });
+  const tariffSlider1 = connectSlider1 && nouislider.create(connectSlider1, {
+    tooltips: true,
+    connect: [ true, false ],
+    step: 1,
+    start: 10,
+    range: {
+      "min": 0,
+      "max": 40
+    },
+    format: {
+      to: (value) => value,
+      from: (value) => Math.round(Number(value)),
+    },
+    pips: {
+      mode: "range",
+      density: 400,
+    },
+  });
 
-    const tariffSlider2 = nouislider.create(connectSlider2, {
-      tooltips: true,
-      connect: [ true, false ],
-      step: 1,
-      start: 2,
-      range: {
-        "min": 0,
-        "max": 5
-      },
-      format: {
-        to: (value) => value,
-        from: (value) => Math.round(Number(value)),
-      },
-      pips: {
-        mode: "range",
-        density: 400,
-      },
-    });
+  const tariffSlider2 = connectSlider2 && nouislider.create(connectSlider2, {
+    tooltips: true,
+    connect: [ true, false ],
+    step: 1,
+    start: 2,
+    range: {
+      "min": 0,
+      "max": 5
+    },
+    format: {
+      to: (value) => value,
+      from: (value) => Math.round(Number(value)),
+    },
+    pips: {
+      mode: "range",
+      density: 400,
+    },
+  });
 
-    tariffSlider1.on("update", (value) => connectInput1.value = value);
-    tariffSlider2.on("update", (value) => connectInput2.value = value);
-  }
+  (tariffSlider1 && connectInput1) && tariffSlider1.on("update", (value) => connectInput1.value = value);
+  (tariffSlider2 && connectInput2) && tariffSlider2.on("update", (value) => connectInput2.value = value);
 })
