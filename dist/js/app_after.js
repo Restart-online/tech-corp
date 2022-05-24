3ea9097945ec57d85a7cad08dd940b7f749fd21b
Mau0HeZzz <mau0hezzz@hotmail.com>
2022-05-25 02:45:50 +0300

    finish #1

--- dist/js/app_after.js
+++ dist/js/app_after.js
@@ -16667,26 +16667,49 @@ PERFORMANCE OF THIS SOFTWARE.
             };
             return this.init();
         }
-        const historySelector = ".js-history__slider";
+        const historySliderSelector = ".js-history__slider";
         const historyTextSelector = ".js-history-slider__text";
         document.addEventListener("DOMContentLoaded", (() => {
             const slidesText = document.querySelectorAll(historyTextSelector);
             slidesText.forEach((item => item.dataset.text = item.textContent.trim()));
-            new core(historySelector, {
-                loop: true,
-                loopedSlides: 20,
+            new core(historySliderSelector, {
+                modules: [ Navigation ],
+                centeredSlides: true,
                 slidesPerView: 5,
+                initialSlide: 2,
                 watchSlidesProgress: true,
                 slideActiveClass: "history-slider__slide--active",
                 slideVisibleClass: "history-slider__slide--visible",
                 wrapperClass: "history-slider__wrapper",
                 watchSlidesVisibility: true,
+                navigation: {
+                    nextEl: ".history-slider__arrow--next",
+                    prevEl: ".history-slider__arrow--prev"
+                },
                 on: {
                     init: swiper => {
                         swiper.slides.forEach(((slide, index) => {
                             const year = slide.querySelector(".history-slider__year");
-                            year.addEventListener("click", (() => swiper.slideToLoop(index)));
+                            year.addEventListener("click", (() => swiper.slideTo(index)));
                         }));
+                    }
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 10
+                    },
+                    480: {
+                        slidesPerView: 3,
+                        spaceBetween: 10
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 10
+                    },
+                    1200: {
+                        slidesPerView: 5,
+                        spaceBetween: 0
                     }
                 }
             });
